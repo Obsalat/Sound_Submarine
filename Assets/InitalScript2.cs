f17a67d51a8a0f326c701b9a19b3de800bc5c7cb
Labtecs <labtecs@vivaldi.net>
2020-06-30 16:04:01 +0200

    q
    Inital Script

---
+++ Assets/InitalScript2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitalScript2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is a Project by Oliver and Christian");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
